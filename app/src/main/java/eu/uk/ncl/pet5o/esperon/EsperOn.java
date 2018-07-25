80ac1b0f81fbe36ddafa3b46a7a914911e3328f1
Peter Michalák <P.Michalak1@newcastle.ac.uk>
2018-07-25 15:49:39 +0100

    simple init

--- app/src/main/java/eu/uk/ncl/pet5o/esperon/EsperOn.java
+++ app/src/main/java/eu/uk/ncl/pet5o/esperon/EsperOn.java
@@ -3,7 +3,13 @@ package eu.uk.ncl.pet5o.esperon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import eu.uk.ncl.pet5o.esper.client.Configuration;
+import eu.uk.ncl.pet5o.esper.client.EPServiceProvider;
+import eu.uk.ncl.pet5o.esper.client.EPServiceProviderManager;
+import eu.uk.ncl.pet5o.esper.client.EPStatement;
 
 public class EsperOn extends AppCompatActivity {
 
@@ -14,6 +20,17 @@ public class EsperOn extends AppCompatActivity {
 
         // init esper
         Configuration config = new Configuration();
+        EPServiceProvider epService = EPServiceProviderManager.getDefaultProvider(config);
+        EPStatement epStatement = epService.getEPAdministrator().createEPL("SELECT * FROM AccelEvent");
+        epStatement.setSubscriber(null);
 
+        Map<String, Object> event = new HashMap<String, Object>();
+        event.put("ts", System.currentTimeMillis());
+        event.put("x", 1);
+        event.put("y", 2);
+        event.put("z", 3);
+
+        // send event to Esper
+        epService.getEPRuntime(\).sendEvent(event, "AccelEvent");
     }
 }
